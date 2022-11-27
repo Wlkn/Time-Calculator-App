c99f1ee05576e40116ee0fd460a9fe28858a7680
Wlkn <alibachar27@gmail.com>
2022-11-27 00:02:00 -0500

    functional remove Button

--- src/components/Buttons/OperatorButton.jsx
+++ src/components/Buttons/OperatorButton.jsx
@@ -1,24 +1,34 @@
 import Button from "./Button";
 
-export default function OperatorButton({ value, setScreenState, setLastUsedBtnState }) {
+export default function OperatorButton({
+    value,
+    setScreenState,
+    setLastUsedBtnState,
+}) {
     return (
         <Button
             className="operator-btn"
             value={value}
             key={value}
             onClick={() => {
-                setLastUsedBtnState(
-                    lastUsedBtnState => {
-                        setScreenState(state => {
-                            if (lastUsedBtnState === "O") {
-                                return state.substring(0, state.length - 3) + " " + value + " ";
-                            }
-                            return state + " " + value + " ";
-                        });
-                        return "O";
-                    }
-                );
-                
+                setLastUsedBtnState((lastUsedBtnState) => {
+                    setScreenState((state) => {
+                        console.log(lastUsedBtnState);
+                        if (lastUsedBtnState === "O") {
+                            // console.log("it worked");
+                            return (
+                                state.substring(0, state.length - 3) +
+                                " " +
+                                value +
+                                " "
+                            );
+                            // }else if (lastUsedBtnState === "R") {
+                            //     return (state + " " + value + " ")
+                        }
+                        return state + " " + value + " ";
+                    });
+                    return "O";
+                });
             }}
         ></Button>
     );
--- src/components/Buttons/RemoveButton.jsx
+++ src/components/Buttons/RemoveButton.jsx
@@ -1,16 +1,27 @@
 import Button from "./Button";
 
-export default function RemoveButton({ setScreenState }) {
+export default function RemoveButton({ setScreenState, setLastUsedBtnState }) {
     return (
         <Button
             className="Remove-btn"
             value={"←"}
             key={"←"}
             onClick={() => {
+                setLastUsedBtnState("R");
                 setScreenState((state) => {
+                    let removeDigitNumber = state.substring(
+                        0,
+                        state.length - 1
+                    );
+                    let removeDigitOperator = state.substring(
+                        0,
+                        state.length - 3
+                    );
                     if (state.slice(-1) === " ") {
-                        return state.slice(0, -3);
+                        console.log(`state: ${state}`);
+                        return removeDigitOperator;
                     }
+                    return removeDigitNumber;
                 });
             }}
         ></Button>
